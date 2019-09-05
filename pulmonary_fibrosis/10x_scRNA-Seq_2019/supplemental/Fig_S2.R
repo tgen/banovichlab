c0cfc69fa9d2d7c9418bb8ee5e4bba75b9a56d5d
Austin-Gutierrez <agutierrez@tgen.org>
2019-09-05 11:18:41 -0700

    Create Fig_S2

---
+++ pulmonary_fibrosis/10x_scRNA-Seq_2019/supplemental/Fig_S2.R
@@ -0,0 +1,28 @@
+# ======================================
+# Load libraries
+# ======================================
+library(Seurat)
+
+# ======================================
+# Read in Seurat object
+# ======================================
+ild <- readRDS("ILD.rds")
+epi <- readRDS("Epithelial.rds")
+endo <- readRDS("Endothelial.rds")
+immune <- readRDS("Immune.rds")
+meso <- readRDS("Mesochymal.rds")
+
+# ======================================
+# Figure S: 2A
+# ======================================
+FeaturePlot(ild, c("PTPRC",
+                   "EPCAM",
+                   "PECAM1"))
+
+# ======================================
+# Figure S: 2C
+# ======================================
+ElbowPlot(endo)
+ElbowPlot(epi)
+ElbowPlot(immune)
+ElbowPlot(meso)
